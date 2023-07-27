e7becc97559c3b594e49b5207e1409c096ef7bd6
yuseop <mrlys24@naver.com>
2023-07-27 11:14:59 +0900

    7/27 test revive

--- TEST7_NOTICE_DUMMY.sql
+++
@@ -1,7 +0,0 @@
--- noti
-INSERT INTO notice VALUES(NOTI_SEQ.NEXTVAL,'공지1','공지내용1','admin','spring.png',default);
-INSERT INTO notice VALUES(NOTI_SEQ.NEXTVAL,'공지2','공지내용2','admin','spring.png',default);
-INSERT INTO notice VALUES(NOTI_SEQ.NEXTVAL,'공지3','공지내용3','admin','spring.png',default);
-INSERT INTO notice VALUES(NOTI_SEQ.NEXTVAL,'공지3','공지내용3','admin','spring.png',default);
-
-commit;
